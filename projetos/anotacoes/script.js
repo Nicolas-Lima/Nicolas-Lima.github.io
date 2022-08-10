42f1018ece5e46af86009ba1785a7ac935a4e2bb
Nícolas Lima <nicolasdelimaalves00@gmail.com>
2022-08-10 16:10:34 -0300

    att

---
+++ projetos/anotacoes/script.js
@@ -0,0 +1,69 @@
+const PDFsName = {
+    html: "HTML",
+    css: "CSS",
+    fundamentos: "Fundamentos",
+    estruturasDeControle: "Estruturas de controle",
+};
+
+const pdf = document.querySelector("#pdf");
+
+const inputHeightPDF = document.querySelector("#heightPDF");
+const inputWidthPDF = document.querySelector("#widthPDF");
+
+inputHeightPDF.addEventListener("input", (e) => {
+    const typedValue = inputHeightPDF.value;
+
+    if(isNaN(typedValue)) {
+        inputHeightPDF.value = "";
+        inputHeightPDF.setAttribute("placeholder", "O valor digitado não é um número!");
+    }
+
+    else {
+        pdf.style.height = `${typedValue}px`;
+    };
+});
+
+inputWidthPDF.addEventListener("input", () => {
+    const typedValue = inputWidthPDF.value;
+
+    if(isNaN(typedValue)) {
+        inputWidthPDF.value = "";
+        inputWidthPDF.setAttribute("placeholder", "O valor digitado não é um número!");
+    }
+
+    else {
+        pdf.style.width = `${typedValue}px`;
+    };
+});
+
+const switchPDF = document.querySelector("#switchPDF");
+
+const buttonsSwitchPDF = switchPDF.querySelectorAll("button[value]");
+
+buttonsSwitchPDF.forEach(buttonSwitchPDF => {
+    buttonSwitchPDF.addEventListener("click", () => {
+        const buttonSwitchPDFValue = buttonSwitchPDF.value;
+
+        function changePDFData(path) {
+            pdf.setAttribute("data", path);
+        };
+
+        function changePDFTitle(PDFName) {
+            const PDFTitle = document.querySelector("#PDFTitle");
+            PDFTitle.innerText = PDFName;
+        };
+
+        let PDFPath;
+
+        if(buttonSwitchPDF.hasAttribute("javascriptPDF")) {
+            PDFPath = `pdfs/javascript/${buttonSwitchPDFValue}.pdf`;
+        }
+        
+        else {
+            PDFPath = `pdfs/${buttonSwitchPDF.value}.pdf`;
+        };
+
+        changePDFData(PDFPath);
+        changePDFTitle(PDFsName[buttonSwitchPDFValue]);
+    });
+});
