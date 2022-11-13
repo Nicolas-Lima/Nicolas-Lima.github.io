b36ae5551de8f3193c6f2db83f47007b0a32ce41
Nicolas-Lima <nicolasdelimaalves00@gmail.com>
2022-11-13 15:19:47 -0300

    att

--- projetos/quiz1/js/script.js
+++ projetos/quiz1/js/script.js
@@ -14,11 +14,55 @@ const currentQuestionIndex = {
     
     else if(_value == this._value - 1) {
       this._value--;
-    };
+    }
   }
-};
+}
 
 const questions = [
+  {
+    "questionTitle": "Qual é o 2?",
+    "answers": {
+      "a": "3",
+      "b": "2",
+      "c": "4",
+      "d": "1",
+      "e": ""
+    },
+    "rightAnswerLetter": "b",
+    "rightAnswerClickedBefore": false,
+    "image": {
+      "classes": {
+        "mx-auto": true,
+        "d-block": true,
+        "mt": 5,
+        "mb": 0
+      },
+      "imageName": "avenue.jpg",
+      "maxWidth": "80%"
+    }
+  },
+    {
+    "questionTitle": "Qual é o 2?",
+    "answers": {
+      "a": "3",
+      "b": "2",
+      "c": "4",
+      "d": "1",
+      "e": ""
+    },
+    "rightAnswerLetter": "b",
+    "rightAnswerClickedBefore": false,
+    "image": {
+      "classes": {
+        "mx-auto": true,
+        "d-block": true,
+        "mt": 5,
+        "mb": 0
+      },
+      "imageName": "avenue.jpg",
+      "maxWidth": "80%"
+    }
+  },
   {
     "questionTitle": "Qual o seu nome?",
     "answers": {
@@ -55,6 +99,18 @@ const questions = [
     "rightAnswerLetter": "a",
     "rightAnswerClickedBefore": false
   },
+  {
+    "questionTitle": "Qual é a maior parte do olho?",
+    "answers": {
+      "a": "Retina",
+      "b": "Oculos",
+      "c": "Calcanhar",
+      "d": "Globo ocular",
+      "e": "Sobrancelha"
+    },
+    "rightAnswerLetter": "d",
+    "rightAnswerClickedBefore": false
+  },
 ];
 
 // Dark mode
@@ -79,27 +135,120 @@ btnDarkMode.addEventListener("click", () => {
     toggleDNoneClass(lightBulbSvg);
     toggleDNoneClass(lightBulbOffSvg);
     toggleActiveClass(btnDarkMode);
-  };
+  }
   
   function toggleBgDarkClass(element) {
     element.classList.toggle("bg-dark");
-  };
+  }
   
   function toggleDNoneClass(element) {
     element.classList.toggle("d-none");
-  };
+  }
   
   function toggleActiveClass(element) {
     element.classList.toggle("active");
-  };
+  }
 });
 
 // Dark mode end
 
+// Creating the question
+
+function createQuestion() {
+  const hasImage = board.contains(document.querySelector("#divImage"));
+  if(hasImage) removeImage();
+
+  const currentQuestion = getCurrentQuestion();
+  
+  // Trocar ESTA LOGICA
+  
+  const hasMoreQuestions = currentQuestion instanceof Object;
+  
+  if(hasMoreQuestions) {
+    const currentQuestion = getCurrentQuestion();
+    const {questionTitle, answers, rightAnswerLetter, image} = currentQuestion;
+    const hasImage = image || false;
+    
+    createQuestionTitle(questionTitle, rightAnswerLetter);
+
+    if(hasImage) {
+      createImage();
+    }
+    
+    answersKeys = Object.keys(answers);
+    answersKeys.forEach(letter => {
+      const answer = answers[letter];
+      
+      if(answer.length > 0) {
+        createAnswer(letter, answer);
+      }
+    });
+  }
+  
+  else if(!hasMoreQuestions) {
+    addInvisibleClass("nextQuestion");
+    
+    const divAnswers = board.querySelector("#answers");
+    const h1 = createElement("h1");
+    h1.innerText = "Parábens! Você chegou ao final do quiz!";
+    h1.classList.add("text-success", "text-center");
+    
+    cleanFields();
+    divAnswers.appendChild(h1);
+  }
+}
+
 function createQuestionTitle(questionTitle) {
   const _questionTitle = board.querySelector("#questionTitle");
   _questionTitle.innerText = questionTitle;
-};
+}
+
+function createImage() {
+  const alreadyHasImage = board.contains(document.querySelector("#divImage"));
+  
+  if(!alreadyHasImage) {
+    const {image} = getCurrentQuestion();
+    const imageClasses = image.classes;
+
+    const divImage = createElement("div");
+    divImage.id = "divImage";
+    
+    const img = createElement("img");
+    img.id = "image";
+    
+    // Max Size
+    
+    img.style.maxWidth = image.maxWidth;
+    
+    // Path
+    
+    const imagePath = `imgs/${image.imageName}`;
+    img.src = imagePath;
+    
+    // Adding classes
+    
+    for(let imageClass in imageClasses) {
+      if(imageClasses[imageClass] === true) {
+        img.classList.add(imageClass);
+      }
+      
+      else {
+        const classValue = imageClasses[imageClass];
+        img.classList.add(`${imageClass}-${classValue}`);
+      }
+    }
+    
+    divImage.appendChild(img);
+    board.appendChild(divImage);
+    const divAnswers = document.querySelector("#answers");
+    board.insertBefore(divImage, divAnswers);
+  }
+}
+
+function removeImage() {
+  const divImage = document.querySelector("#divImage");
+  board.removeChild(divImage);
+}
 
 // Div actions
 
@@ -107,7 +256,17 @@ const divActions = board.querySelector("#actions");
 const btnNext = divActions.querySelector("#nextQuestion");
 const btnPrevious = divActions.querySelector("#previousQuestion");
 
-btnNext.addEventListener("click", nextQuestion);
+btnNext.addEventListener("click", () => {
+  addInvisibleClass("previousQuestion");
+  addInvisibleClass("nextQuestion");
+  board.style.transform = "scale(0.7)";
+
+  setTimeout(() => {
+    nextQuestion();
+    removeInvisibleClass("previousQuestion");
+    removeInvisibleClass("nextQuestion");
+  }, 1000);
+});
 btnPrevious.addEventListener("click", previousQuestion);
 
 function createAnswer(letter, answer) {
@@ -133,7 +292,7 @@ function createAnswer(letter, answer) {
   const currentQuestion = getCurrentQuestion();
   const {rightAnswerLetter} = currentQuestion;
   const isRightAnswer = letter == rightAnswerLetter;
-  const rightAnswerClickedBefore = currentQuestion.rightAnswerClickedBefore == true;
+  const rightAnswerClickedBefore = currentQuestion.rightAnswerClickedBefore === true;
   
   if(rightAnswerClickedBefore) {
     btnAnswer.setAttribute("disabled", "");
@@ -144,21 +303,20 @@ function createAnswer(letter, answer) {
     }
     
     else {
-      //btnAnswer.classList.remove("btn-outline-primary");
       addBtnDangerClass(btnAnswer);
-    };
+    }
   }
   
   else if (!rightAnswerClickedBefore) {
     addBtnOutlinePrimaryClass(btnAnswer);
-  };
+  }
   
   // Button action
   
   btnAnswer.addEventListener("click", () => {
   	checkAnswer(letter);
   });
-};
+}
 
 function checkAnswer(letter) {
   const currentQuestion = getCurrentQuestion();
@@ -171,88 +329,69 @@ function checkAnswer(letter) {
   
   else if(!isRightAnswer) {
     wrongAnswer(letter);
-  };
-};
+  }
+}
 
 function rightAnswer(letter) {
+  const btnAnswer = board.querySelector(`button[value="${letter}"]`)
   const currentQuestion = getCurrentQuestion();
   const {rightAnswerLetter} = currentQuestion;
   questions[currentQuestionIndex.value].rightAnswerClickedBefore = true;
 
-  nextQuestion();
-};
+  addBtnSuccessClass(btnAnswer);
+
+  const btnsAnswers = board.querySelectorAll("#answers > button");
+  btnsAnswers.forEach(btn => {
+    const letter = btn.getAttribute("value");
+    if(letter !== rightAnswerLetter) {
+      addBtnDangerClass(btn);
+    }
+  });
+
+  addInvisibleClass("previousQuestion");
+  addInvisibleClass("nextQuestion");
+  board.style.transform = "scale(0.7)";
+
+  setTimeout(() => {
+    nextQuestion();
+    removeInvisibleClass("previousQuestion");
+    removeInvisibleClass("nextQuestion");
+  }, 1000);
+}
 
 function wrongAnswer(letter) {
   const btnAnswer = board.querySelector(`button[value="${letter}"]`)
   addBtnDangerClass(btnAnswer);
-};
+}
 
 function createElement(type) {
   return document.createElement(type);
-};
+}
 
 function addBtnOutlinePrimaryClass(element) {
   element.classList.remove("btn-danger", "btn-success");
   element.classList.add("btn-outline-primary");
-};
+}
 
 function addBtnDangerClass(element) {
   element.classList.remove("btn-outline-primary", "btn-success");
   element.classList.add("btn-danger");
-};
+}
 
 function addBtnSuccessClass(element) {
   element.classList.remove("btn-outline-primary", "btn-danger");
   element.classList.add("btn-success");
-};
+}
 
 function addInvisibleClass(id) {
   const element = document.getElementById(id);
   element.classList.add("invisible");
-};
+}
 
 function removeInvisibleClass(id) {
   const element = document.getElementById(id);
   element.classList.remove("invisible");
-};
-
-// Creating the question
-
-function createQuestion() {
-  const currentQuestion = getCurrentQuestion();
-  
-  // Trocar ESTA LOGICA
-  
-  const hasMoreQuestion = currentQuestion instanceof Object;
-  
-  if(hasMoreQuestion) {
-    const currentQuestion = getCurrentQuestion();
-    const {questionTitle, answers, rightAnswerLetter} = currentQuestion;
-    
-    createQuestionTitle(questionTitle, rightAnswerLetter);
-    
-    answersKeys = Object.keys(answers);
-    answersKeys.forEach(letter => {
-      const answer = answers[letter];
-      
-      if(answer.length > 0) {
-        createAnswer(letter, answer);
-      };
-    });
-  }
-  
-  else if(!hasMoreQuestion) {
-    addInvisibleClass("nextQuestion");
-    
-    const divAnswers = board.querySelector("#answers");
-    const h1 = createElement("h1");;
-    h1.innerText = "Parábens! Você chegou ao final do quiz!";
-    h1.classList.add("text-success", "text-center");
-    
-    cleanFields();
-    divAnswers.appendChild(h1);
-  };
-};
+}
 
 function cleanFields() {
   const questionTitle = board.querySelector("#questionTitle");
@@ -260,25 +399,27 @@ function cleanFields() {
   
   const divAnswers = board.querySelector("#answers");
   divAnswers.innerHTML = "";
-};
+}
 
 function getCurrentQuestion() {
   const currentQuestion = questions[currentQuestionIndex.value];
 
   return currentQuestion;
-};
+}
 
 function nextQuestion() {
   cleanFields();
   increaseQuestionIndex();
   createQuestion();
+
+  board.style.transform = "scale(1)";
   
   const _isSecondQuestion = currentQuestionIndex.value == 1;
   
   if(_isSecondQuestion) {
     removeInvisibleClass("previousQuestion");
-  };
-};
+  }
+}
 
 function previousQuestion() {
   cleanFields();
@@ -289,37 +430,37 @@ function previousQuestion() {
   
   if(_isFirstQuestion) {
     addInvisibleClass("previousQuestion");
-  };
+  }
   
   const btnNext = board.querySelector("#nextQuestion");
   const isBtnNextInvisible = btnNext.classList.contains("invisible");
 
   if(isBtnNextInvisible) {
     removeInvisibleClass("nextQuestion");
-  };
-};
+  }
+}
 
 function increaseQuestionIndex() {
   currentQuestionIndex.value++;
-};
+}
 
 function decreaseQuestionIndex() {
   if(currentQuestionIndex.value >= 1) {
     currentQuestionIndex.value--;
-  };
-};
+  }
+}
 
 function isFirstQuestion() {
   const _isFirstQuestion = currentQuestionIndex.value == 0;
   
   return _isFirstQuestion;
-};
+}
 
 function isLastQuestion() {
   const questionsLength = questions.length;
   const _isLastQuestion = currentQuestionIndex.value == questionsLength - 1;
 
   return _isLastQuestion;
-};
+}
 
 window.addEventListener("load", createQuestion);
