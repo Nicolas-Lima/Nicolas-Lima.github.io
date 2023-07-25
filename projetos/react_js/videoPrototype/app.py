e2dbb0a01a17cf796d9bdac4d83fc82e640a939a
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-24 22:09:47 -0300

    Removido todos os arquivos para usar o React JS CDN em vez do Javascript puro

--- projetos/react_js/videoPrototype/app.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# Defina o caminho completo para o arquivo index.py
-caminho_arquivo = r'C:\Users\Nicolas\Dropbox\Programacao\React JS 2\projetos\videoPrototype\index.py'
-
-# Use a função subprocess.Popen para executar o arquivo com o interpretador python em segundo plano
-
-subprocess.Popen(['python'], creationflags=subprocess.CREATE_NO_WINDOW)
-subprocess.Popen(['python', caminho_arquivo], creationflags=subprocess.CREATE_NO_WINDOW)
