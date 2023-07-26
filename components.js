991ff6b49b51723048ffcd8b13f88ce398144ccc
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-26 15:41:31 -0300

    Modificações feitas no site

---
+++ components.js
@@ -0,0 +1,448 @@
+const { useState, useEffect } = React;
+
+function Skills() {
+  return (
+    <div className="mb-3 mt-5" id="skills">
+      <article className="mb-2-3rem p-3 d-flex justify-content-center align-items-center m-0 border border-light border-opacity-10">
+        <h1 className="m-0 text-center">Minhas habilidades</h1>
+      </article>
+      <div className="ps-3 ps-sm-0">
+        <div className="category mt-2-3rem">
+          <h3 className="text-light-blue category-title">Front-end</h3>
+          <div className="grid">
+            <div className="mb-3 mb-lg-0" data-tooltip="Intermediário">
+              <div className="d-flex align-items-center">
+                <HtmlIcon />
+                <span className="ms-2">HTML</span>
+              </div>
+              <progress
+                value="2"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div className="mb-3 mb-lg-0" data-tooltip="Iniciante">
+              <div className="d-flex align-items-center">
+                <CssIcon />
+                <span className="ms-2">CSS</span>
+              </div>
+
+              <progress
+                value="1"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div className="mb-3 mb-lg-0" data-tooltip="Intermediário">
+              <div className="d-flex align-items-center">
+                <JsIcon />
+                <span className="ms-2">Javascript</span>
+              </div>
+
+              <progress
+                value="2"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+          </div>
+        </div>
+        <div className="category mt-2-3rem">
+          <h3 className="text-light-blue category-title">Front-end Frameworks</h3>
+
+          <div className="grid">
+            <div className="mb-3 mb-lg-0" data-tooltip="Intermediário">
+              <div className="d-flex align-items-center">
+                <ReactIcon />
+                <span className="ms-2">React JS</span>
+              </div>
+              <progress
+                value="2"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div className="mb-3 mb-lg-0" data-tooltip="Intermediário">
+              <div className="d-flex align-items-center">
+                <BootstrapIcon />
+                <span className="ms-2">Bootstrap</span>
+              </div>
+              <progress
+                value="2"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div>
+              <div className="d-flex align-items-center"></div>
+            </div>
+          </div>
+        </div>
+        <div className="category mt-2-3rem">
+          <h3 className="text-light-blue category-title">Databases</h3>
+
+          <div className="grid">
+            <div className="mb-3 mb-lg-0" data-tooltip="Intermediário">
+              <div className="d-flex align-items-center">
+                <MySqlIcon />
+                <span className="ms-2">
+                  MySql ( <strong>SQL</strong> )
+                </span>
+              </div>
+              <progress
+                value="2"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div>
+              <div className="d-flex align-items-center"></div>
+            </div>
+            <div>
+              <div className="d-flex align-items-center"></div>
+            </div>
+          </div>
+        </div>
+        <div className="category mt-2-3rem">
+          <h3 className="text-light-blue category-title">Back-end</h3>
+          <div className="grid">
+            <div className="mb-3 mb-lg-0" data-tooltip="Iniciante">
+              <div className="d-flex align-items-center">
+                <PythonIcon />
+                <span className="ms-2">Python</span>
+              </div>
+              <progress
+                value="1"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div className="mb-3 mb-lg-0" data-tooltip="Iniciante">
+              <div className="d-flex align-items-center">
+                <NodeJSIcon />
+                <span className="ms-2">Node JS</span>
+              </div>
+              <progress
+                value="1"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div>
+              <div className="d-flex align-items-center"></div>
+            </div>
+          </div>
+        </div>
+        <div className="category mt-2-3rem">
+          <h3 className="text-light-blue category-title">Tools</h3>
+          <div className="grid">
+            <div className="mb-3 mb-lg-0" data-tooltip="Intermediário">
+              <div className="d-flex align-items-center">
+                <GitIcon />
+                <span className="ms-2">GIT</span>
+              </div>
+              <progress
+                value="2"
+                max="3"
+                className="skillLevel  mt-2"></progress>
+            </div>
+            <div>
+              <div className="d-flex align-items-center"></div>
+            </div>
+            <div>
+              <div className="d-flex align-items-center"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  const [mailIconContainerTooltip, setMailIconContainerTooltip] =
+    useState("Copiar e-mail");
+  const email = "nicolasdelimaalves00@gmail.com";
+
+  const copyEmailToClipboard = () => {
+    navigator.clipboard.writeText(email).then(() => {
+      setMailIconContainerTooltip("E-mail copiado!");
+    });
+  };
+
+  return (
+    <div>
+      <h2 className="mb-3">Nícolas Lima</h2>
+      <h3 className="text-light-green mb-4">
+        Estudante de Desenvolvimento Front-End React JS
+      </h3>
+      <div className="fs-5">
+        <div className="welcomeMessage text-light-blue mb-3">
+          <span className="me-2">👋🏼</span> Olá, seja bem vindo ao meu
+          portfólio!
+        </div>
+        <div>
+          Sou um desenvolvedor apaixonado pelo que faço e, nos últimos 7
+          meses, tenho me dedicado intensamente a aprimorar minhas
+          habilidades, com foco especial em{" "}
+          <span className="text-light-green">React JS</span> e{" "}
+          aprofundamento em{" "}
+          <span className="text-light-green">JavaScript</span>.
+        </div>
+
+        <div className="mt-3">
+          Abaixo, apresentarei minhas{" "}
+          <a
+            href="#skills"
+            className="link-without-formatting text-light-green text-hover-light-blue">
+            <span className="me-1 cursor-hover-pointer">habilidades</span>
+          </a>
+          e também compartilharei algum dos{" "}
+          <a
+            href="#projects"
+            className="link-without-formatting text-light-green text-hover-light-blue">
+            <span className="cursor-hover-pointer">projetos</span>
+          </a>{" "}
+          que desenvolvi.
+        </div>
+      </div>
+      <div
+        className="mt-3 mailIcon-container w-auto d-inline-block"
+        data-tooltip={mailIconContainerTooltip}
+        onMouseLeave={() => setMailIconContainerTooltip("Copiar e-mail")}
+        onClick={copyEmailToClipboard}>
+        <MailIcon />
+        <span className="ms-2 email">
+          {email}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <nav className="mb-4 mb-md-0 border-light border-bottom border-opacity-10 d-flex justify-content-around py-1">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/nicolas-lima"
+            className="link-light remove-focus-background"
+            id="githubLink"
+            target="_blank"
+            aria-label="Menu">
+            <GithubIcon />
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <strong className="fs-5">Nícolas Lima</strong>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a
+            href="https://linkedin.com/in/nícolaslima"
+            className="link-info remove-focus-background"
+            id="linkedinLink"
+            target="_blank"
+            aria-label="Linkedin">
+            <LinkedinIcon />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Projects({ toggleModalLinks }) {
+  const projects = [
+    {
+      technology: "React JS",
+      projects: [
+        {
+          fullName: "Site do meu TCC ( Em Desenvolvimento )",
+          name: "Site do meu TCC",
+          projectUrl: "https://sitetccinfo.netlify.app/",
+          githubUrl: "https://github.com/Nicolas-Lima/site_tcc",
+        },
+        {
+          fullName: "Loja de produtos ( Em Desenvolvimento )",
+          name: "Loja de produtos",
+          projectUrl: "https://a-product-store.netlify.app/",
+          githubUrl: "https://github.com/Nicolas-Lima/product-store",
+        },
+        {
+          fullName: "Prime Flix",
+          name: "Prime Flix",
+          projectUrl: "https://rdmetas.000webhostapp.com/",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-React-JS/tree/main/Prime%20Flix",
+        },
+      ],
+    },
+    {
+      technology: "Javascript Vanilla",
+      projects: [
+        {
+          fullName: "Clock App",
+          name: "Clock App",
+          projectUrl: "projetos/clock_app/index.html",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-JS/tree/main/Clock%20App",
+        },
+        {
+          fullName: "Primeira Game Engine",
+          name: "Primeira Game Engine",
+          projectUrl: "projetos/game_engine_1/index.html",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-JS/tree/main/Game%20Engine%201",
+        },
+        {
+          fullName: "Segunda Game Engine",
+          name: "Segunda Game Engine",
+          projectUrl: "projetos/game_engine_2/index.html",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-JS/tree/main/Game%20Engine%202",
+        },
+        {
+          fullName: "Jogo da velha",
+          name: "Jogo da velha",
+          projectUrl: "projetos/jogoVelha/index.html",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-JS/tree/main/Joguinho%20da%20velha",
+        },
+        {
+          fullName: "To-do List 1",
+          name: "To-do List 1",
+          projectUrl: "https://nicolas-listatarefas.herokuapp.com/",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-JS/tree/main/To-do%20List%201",
+        },
+        {
+          fullName: "To-do List 2",
+          name: "To-do List 2",
+          projectUrl: "projetos/to-do-lists/To-do-List-2/index.html",
+          githubUrl:
+            "https://github.com/Nicolas-Lima/Projetos-JS/tree/main/To-do%20List%202",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="mb-3 mt-4 mt-md-5" id="projects">
+      <article className="mb-4 p-3 d-flex justify-content-center align-items-center m-0 border border-light border-opacity-10">
+        <h1 className="m-0">Projetos</h1>
+      </article>
+      {projects.map(technology => {
+        return (
+          <div className="row mb-4" key={technology.technology}>
+            <h1 className="mb-3">{technology.technology}</h1>
+            {technology.projects.map(project => {
+              return (
+                <Project
+                  projectFullname={project.fullName}
+                  projectName={project.name}
+                  projectUrl={project.projectUrl}
+                  githubUrl={project.githubUrl}
+                  toggleModalLinks={toggleModalLinks}
+                  key={project.name}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Project({
+  projectFullname,
+  projectName,
+  projectUrl,
+  githubUrl = "",
+  toggleModalLinks,
+}) {
+  return (
+    <div className="col-sm-12 col-md-6 col-lg-4">
+      <article className="project">
+        <header className="text-center mb-5">{projectFullname}</header>
+        <button
+          type="button"
+          className="btn btn-outline-primary px-3 py-2 m-2"
+          onClick={() =>
+            toggleModalLinks({ projectName, projectUrl, githubUrl })
+          }>
+          Acessar
+        </button>
+      </article>
+    </div>
+  );
+}
+
+function LinksModal({ toggleModalLinks }) {
+  return (
+    <dialog id="modal-links" className="p-3">
+      <article className="d-flex flex-column align-items-center">
+        <a
+          onClick={toggleModalLinks}
+          aria-label="Close"
+          className="close"></a>
+
+        <h3 id="projectName">Acessar</h3>
+        <div className="d-flex flex-column align-items-center w-100 px-5 py-2">
+          <a
+            target="_blank"
+            className="w-100 d-block text-decoration-none"
+            id="projectUrl">
+            <button>
+              <SiteIcon />
+              <span>Site do projeto</span>
+            </button>
+          </a>
+
+          <a
+            className="w-100 d-block text-decoration-none"
+            id="githubUrl"
+            target="_blank">
+            <button className="secondary">
+              <GithubIcon className="mx-2" />
+              <span>Repositório no Github</span>
+            </button>
+          </a>
+        </div>
+      </article>
+    </dialog>
+  );
+}
+
+function ThemeToggler() {
+  const [savedTheme, setSavedTheme] = useState("");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("@savedTheme") || "dark";
+    setSavedTheme(savedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const htmlElement = document.documentElement;
+    const dataTheme = htmlElement.getAttribute("data-theme");
+    htmlElement.setAttribute(
+      "data-theme",
+      dataTheme === "dark" ? "light" : "dark"
+    );
+
+    setSavedTheme(htmlElement.getAttribute("data-theme"));
+
+    localStorage.setItem(
+      "@savedTheme",
+      htmlElement.getAttribute("data-theme")
+    );
+  };
+
+  return (
+    <a
+      className="shadow-none remove-focus-background rounded"
+      id="themeToggler"
+      onClick={toggleTheme}
+      target="_blank"
+      aria-label="theme toggle">
+      {savedTheme == "dark" ? <SunIcon /> : <MoonIcon />}
+    </a>
+  );
+}
---
+++ index.js
@@ -0,0 +1,67 @@
+const { useState, useEffect, useCallback } = React;
+
+function App() {
+  const toggleModalLinks = ({ projectName, projectUrl, githubUrl }) => {
+    const modalLinks = document.getElementById("modal-links");
+    projectName;
+    const projectNameElem = modalLinks.querySelector("#projectName");
+    const projectUrlElem = modalLinks.querySelector("#projectUrl");
+    const githubUrlElem = modalLinks.querySelector("#githubUrl");
+    const isOpen = modalLinks.hasAttribute("open");
+
+    if (isOpen) {
+      document.body.classList.remove("modal-is-open", "modal-is-opening");
+      document.body.classList.add("modal-is-open", "modal-is-closing");
+      modalLinks.removeAttribute("open", "");
+      document.body.classList.remove("modal-is-open", "modal-is-closing");
+      projectUrlElem.classList.remove("d-none");
+      githubUrlElem.classList.remove("d-none");
+      return;
+    }
+
+    projectNameElem.innerText = projectName || "Acessar";
+
+    if (!projectUrl) {
+      projectUrlElem.classList.add("d-none");
+    }
+    if (!githubUrl) {
+      githubUrlElem.classList.add("d-none");
+    }
+
+    try {
+      projectUrlElem.setAttribute("href", projectUrl);
+    } catch (error) {}
+
+    try {
+      githubUrlElem.setAttribute("href", githubUrl);
+    } catch (error) {}
+
+    document.body.classList.add("modal-is-open", "modal-is-opening");
+    modalLinks.setAttribute("open", "");
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("@savedTheme");
+    if (savedTheme === "dark" || savedTheme === "light") {
+      document.documentElement.setAttribute("data-theme", savedTheme);
+    }
+  }, []);
+
+  return (
+    <div className="pb-4 pb-md-0 ps-1 ps-sm-0">
+      <div className="content">
+        <Nav />
+        <div className="container mt-2-3rem">
+          <About />
+          <Skills />
+          <Projects toggleModalLinks={toggleModalLinks} />
+          <LinksModal toggleModalLinks={toggleModalLinks} />
+          {/* <ThemeToggler /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Render the component to the DOM
+ReactDOM.render(<App />, document.getElementById("root"));
